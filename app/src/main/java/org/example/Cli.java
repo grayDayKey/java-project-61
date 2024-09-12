64c06878823d83d65dc2da23bc4747949dd07cce
grayDayKey <8sergey.vyalkin8@gmail.com>
2024-09-12 14:25:58 +0700

    Adds Cli greetings

---
+++ app/src/main/java/org/example/Cli.java
@@ -0,0 +1,18 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class Cli {
+    public static void greet() {
+        final String name;
+
+        System.out.println("Welcome to the Brain Games!");
+        System.out.println("May I have your name?");
+
+        try (Scanner scanner = new Scanner(System.in)) {
+            name = scanner.next();
+        }
+
+        System.out.println("Hello, " + name + "!");
+    }
+}
